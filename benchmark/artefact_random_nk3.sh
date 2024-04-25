370645e403c5f49970abfbffc4553a9a2d4ce6df
Samuel Pastva <sam.pastva@gmail.com>
2024-04-25 08:19:49 +0200

    Add NK3 artefact generator function.

---
+++ benchmark/artefact_random_nk3.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# The purpose of this script is to compute an "artefact" that can be used 
+# to compare the performance of `biobalm` between versions consistently.
+
+# Note that even though we try to limit the evaluation as much as possible,
+# the whole process will still typically require several hours.
+
+# Also note that on laptops (and improperly built desktops :D), the
+# temperature of the environment can have a measurable impact on performance
+# and we are not doing any special variance analysis... always measure
+# in a reasonably consistent conditions.
+
+# The script will setup its own virtual environment in which Python
+# dependencies will be installed.
+
+# This script does not perform any correctness checking, but it could be 
+# a good idea to validate that the results that were computed successfully
+# actually match between versions.
+
+if [[ $(ls _run_*) ]]; then
+    echo "There are existing result runs already present. Please remove them first."
+    exit 2
+fi
+
+# if git diff-index --quiet HEAD --; then
+#     echo "No uncommitted changes. Installing biobalm..."
+# else
+#     echo "There are uncommitted changes. Please commit or stash them."
+#     exit 2
+# fi
+
+rm -rf ./venv
+python3 -m venv ./venv
+
+# Install biobalm and dependencies.
+./venv/bin/pip install biobalm==0.2.0
+
+
+# Apply memory limit.
+# The tested limit was 2^25kB 33_554_432kB ~ 33GB
+if [[ -z "${MEMORY_LIMIT}" ]]; then
+  #echo "Please set env. variable MEMORY_LIMIT appropriate for your system (number, in kB)."
+  #exit 1
+  echo "Memory limit not set. Defaulting to 32GB."
+  MEMORY_LIMIT=33554432
+fi
+
+ulimit -v $MEMORY_LIMIT
+
+# Save the name of the machine, the current git commit and pip list 
+# for future reference.
+git rev-parse HEAD > _run_git_rev.txt
+hostname > _run_hostname.txt
+./venv/bin/pip list > _run_pip_list.txt
+
+MODEL_DIR=../models/random_nk3
+TIMEOUT=1h
+
+set -x
+
+# Benchmark minimal expansion (this ignores motif-avoidant attractors).
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_expand_min.py
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_attractors_min.py
+
+# Benchmark "basic" unconstrained BFS expansion and enumeration.
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_expand_bfs.py
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_attractors_full.py
+
+# Benchmark improved "SCC-reduced" attractor expansion and enumeration.
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_expand_scc.py
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_attractors_scc.py
+
+# Benchmark basic attractor guided expansion and enumeration
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_expand_attr.py
+./venv/bin/python3 run_bench.py $TIMEOUT $MODEL_DIR bench_sd_attractors_attr.py
+
+zip -r perf_random_nk3_`hostname`.zip _run_*

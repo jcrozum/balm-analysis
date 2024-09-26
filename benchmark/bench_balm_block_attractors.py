c6a970f5099df8c114843d4cbec11171ed87ea84
Samuel Pastva <sam.pastva@gmail.com>
2024-09-26 15:39:34 +0200

    Update main benchmark script to latest biobalm

--- benchmark/bench_balm_block_attractors.py
+++ benchmark/bench_balm_block_attractors.py
@@ -17,17 +17,28 @@ config["attractor_candidates_limit"] = 100_000
 
 # Compute the succession diagram.
 sd = SuccessionDiagram(bn, config)
-fully_expanded = sd.expand_block(find_motif_avoidant_attractors=True)
-assert fully_expanded
+fully_expanded = sd.expand_block(
+    find_motif_avoidant_attractors=True,
+    exact_attractor_detection=True,
+    size_limit=1_000,
+)
+
+skipped = 0
+if not fully_expanded:
+    skipped = sd.skip_remaining()
 
 print(f"Succession diagram size:", len(sd))
 print(f"Minimal traps:", len(sd.minimal_trap_spaces()))
+print(f"Skip nodes: {skipped}")
 
 attractor_count = 0
 motif_avoidant_count = 0
 
-for node in sd.expanded_ids():
-    attr = sd.node_attractor_seeds(node, compute=True)
+# The order of iteration is important, because it ensures we start
+# from minimal (or rather last expanded) nodes, i.e. the info from these
+# can be reused in the other methods.
+for node in reversed(list(sd.expanded_ids())):
+    attr = sd.node_attractor_seeds(node, compute=True, symbolic_fallback=True)
     attractor_count += len(attr)
     if not sd.node_is_minimal(node):
         motif_avoidant_count += len(attr)
